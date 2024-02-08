f669689ad5d0e4f4f44f203a04e7cfae522ad806
Charles <charlesnwoye2@gmail.com>
2024-02-08 14:27:57 +0100

    about us fix

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -12,9 +12,9 @@ const About = () => {
 
       <div className='mt-20 md:mt-24 font-Montserrat'>
         <div style={{ backgroundImage: `url(${AboutPhoto})` }} className='flex items-center justify-center m-auto text-center img-bg font-Montserrat'>
-          <p className='flex items-center justify-center w-full h-screen mt-5 text-3xl text-center md:text-4xl text-custom-light bg-black/50'>About Us</p>
+          <p className='flex items-center justify-center w-full h-full pt-5 text-3xl text-center md:text-4xl text-custom-light bg-black/50'>About Us</p>
         </div>
-        <div className='flex flex-col items-start justify-center gap-5 px-4 mt-4 mb-4 md:mt-10 md:flex-row'>
+        <div className='flex flex-col items-start justify-center gap-5 px-4 mb-4 md:mt-10 md:flex-row'>
           <div className='w-64 h-72 bg-custom-green md:w-full text-custom-light'>
             <img src={OwnersPhoto} alt="" className='w-full h-48' loading='lazy' />
             <p className='pt-4 text-sm text-center'><span className='font-BizUdmincho text-xl'>"</span>Konnect Global: Transforming logistics, empowering global success.</p>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import BackgroundImageSlider from './BackgroundImageSlider';
+import HomeMessageForm from './HomeMessageForm';
 
 import planeImg from '../../assets/img/plane.jpg';
 
@@ -162,23 +163,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className='w-full p-5 m-auto bg-custom-blue/20'>
-            <div>
-              <h1 className='text-xl uppercase md:text-4xl'>Leave us a message</h1>
-              <div className='w-16 h-[2px] mt-2 md:mt-5 bg-custom-green'></div>
-            </div>
-
-            <div className='mt-5 md:mt-10'>
-              <div className='flex justify-between mb-5 space-x-5'>
-                <input type="text" placeholder='Name' className='w-full h-8 p-2 outline-none text-custom-green' />
-                <input type="email" placeholder='Email' className='w-full h-8 p-2 outline-none text-custom-green' />
-              </div>
-
-              <textarea type="text" placeholder='Message' className='w-full h-64 p-2 outline-none text-custom-green' />
-
-              <button className='w-24 h-12 p-2 mt-5 text-center transition-all duration-500 ease-in-out text-custom-light bg-custom-green hover:text-custom-green hover:bg-custom-light'>Submit</button>
-            </div>
-          </div>
+          <HomeMessageForm />
         </div>
       </div>
 
